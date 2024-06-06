7724d2cf16f1a0033da0b433411664997181cc07
mahesh-sn <maheshsn705@gmail.com>
2024-06-06 12:24:29 +0530

    Warehouse adding method parameters changed

--- src/main/java/com/jsp/warehouse/controller/WarehouseController.java
+++ src/main/java/com/jsp/warehouse/controller/WarehouseController.java
@@ -2,6 +2,7 @@ package com.jsp.warehouse.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -12,13 +13,13 @@ import com.jsp.warehouse.service.WarehouseService;
 import com.jsp.warehouse.utility.ResponseStructure;
 
 @RestControllerAdvice
-@RequestMapping("/api/version1")
+//@RequestMapping("/api/version1")
 public class WarehouseController {
 	@Autowired
 	private WarehouseService warehouseService;
 	
-	@PostMapping("/warehouses")
-	public String createWarehouse(WarehouseRequset warehouse){
-		return "Warehouse created";
+	@GetMapping("/warehouses")
+	public String createWarehouse(){
+		return "Warehouse Found";
 	}
 }
